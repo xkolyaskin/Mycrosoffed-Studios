3d813e39592a82aa87c5860df5383a22ebdc94d2
Thomas Ashcraft <termisdash@gmail.com>
2025-03-15 08:11:33 -0400

    adding fishing method, doesnt work yet

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -2,11 +2,32 @@ extends CharacterBody2D
 
 func _physics_process(delta):	
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
-	velocity = direction * 600
+	velocity = direction * 200
 	move_and_slide()
 	var overlapping_bodies = %HurtBox.get_overlapping_bodies()
-	if overlapping_bodies.first().has_method("is_fishing"):
-		fish()
+	if overlapping_bodies.size() > 0:
+		var fishing = false
+		if overlapping_bodies[0].has_method("is_fishing"):
+			if !fishing:
+				fish()
+			fishing = true
+			
+	
+
+
 
 func fish():
-	
+	var fishing_interval = Vector2(0.4,0.6)
+	%FishingBar.visible = true
+	var increasing = true
+	var has_caught = false
+	while (!has_caught):
+		await get_tree().create_timer(0.1).timeout
+		if increasing:
+			%FishingBar.value += %FishingBar.value + 1
+			if %FishingBar.value == %FishingBar.max_value:
+				increasing = false;
+		else:
+			%FishingBar.value += %FishingBar.value - 1
+			if %FishingBar.value == %FishingBar.min_value:
+				increasing = true;
