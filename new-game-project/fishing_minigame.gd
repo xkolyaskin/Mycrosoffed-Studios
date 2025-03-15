7bfb1af3ca6f87e052bd7ae2859f45825e0ed952
Thomas Ashcraft <termisdash@gmail.com>
2025-03-15 14:23:36 -0400

    added fishing randomness

--- new-game-project/fishing_minigame.gd
+++ new-game-project/fishing_minigame.gd
@@ -17,12 +17,13 @@ func spawn_fishing_spot():
 	var new_spot = preload("res://fishing_spot.tscn").instantiate()
 	
 	%FishingPath.progress_ratio = randf()
+	%FishingPath.global_position /= 32
+	%FishingPath.global_position *= 32
 	new_spot.global_position = %FishingPath.global_position
 	add_child(new_spot)
 
 func create_crack():
-	var new_spot = preload("res://fishing_spot.tscn").instantiate()
-	
+	var new_spot = preload("res://fishing_crack.tscn").instantiate()
 	%FishingPath.progress_ratio = randf()
 	new_spot.global_position = %FishingPath.global_position
 	add_child(new_spot)
