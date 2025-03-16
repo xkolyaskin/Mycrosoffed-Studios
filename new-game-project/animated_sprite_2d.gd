df3be11600350d1fa187b215502e3c1f21c25fc5
Alex Kolyaskin <akolyask@purdue.edu>
2025-03-16 12:54:21 -0400

    gpt comment

--- new-game-project/animated_sprite_2d.gd
+++ new-game-project/animated_sprite_2d.gd
@@ -1,5 +1,8 @@
 extends AnimatedSprite2D
-
+"""
+Generated from ChatGPT. Prompt:
+	"is there a way to randomize the starting point of an animatedsprite2d that has autoplay on load"
+"""
 func _ready():
 	var total_frames = sprite_frames.get_frame_count(animation)
 	frame = randi() % total_frames
