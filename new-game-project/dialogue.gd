660eec77ffae378df0faaf2caba3e38941c2d182
Thomas Ashcraft <termisdash@gmail.com>
2025-03-15 21:46:59 -0400

    dialogue progress

--- new-game-project/dialogue.gd
+++ new-game-project/dialogue.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
+@onready var dialogue_box = $DialogueBox
+
 func _ready():
-	return
+	while(true):
+		if not dialogue_box.is_typing():
+			dialogue_box.type_text("1st Round: While we do love our meat, a balanced diet requires some veggies too. Walk over to those seeds and choose which one to plant. ")
+		if Input.is_action_just_pressed("interact") and not dialogue_box.is_typing(): 
+			emit_signal("next_text")
+			print("next")
+			
+	
