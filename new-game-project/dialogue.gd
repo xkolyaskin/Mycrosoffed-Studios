8e0c7b66a172bd97cec8490dddfdc1b7476582ec
lukebkastner <143747018+lukebkastner@users.noreply.github.com>
2025-03-16 00:01:16 -0400

    added all story dialogue

--- new-game-project/dialogue.gd
+++ new-game-project/dialogue.gd
@@ -4,8 +4,43 @@ extends Node2D
 @onready var talking_audio = $Talking
 
 var scene = 1
-var dialogue1_1 = "While we do love our meat, a balanced diet requires some veggies too. Walk over to those seeds and choose which one to plant. "
-var dialogue1_2 = "Once they are planted, wait for them to grow and harvest your crops before they go bad. Pick up/place using SPACE or E."
+var dialogueFarm1_1 = "While we do love our meat, a balanced diet requires some veggies too. Walk over to those seeds and choose which one to plant. "
+var dialogueFarm1_2 = "Once they are planted, wait for them to grow and harvest your crops before they go bad. Pick up/place using SPACE or E."
+
+var dialogueFish1_1 = "Salmon is one of the most important foods here, but fishing is no easy work."
+var dialogueFish1_2 = "Walk up to the fishing hole and when indicated, press SPACE or E at the right time to catch a fish.  Catch enough fish and you will succeed."
+
+var dialogueHerd1_1 = "Reindeer herding has sustained our culture for thousands of years, and over this time we have learned to find spots with lots of food for them."
+var dialogueHerd1_2 = "A favorite of theirs is lichen, which grows under the snow around these parts."
+var dialogueHerd1_3 = "Guide them to the green lichen patches and make sure they don’t wander too far."
+
+var dialogueFarm2_1 = "One of the best things the Russians brought was telnoye."
+var dialogueFarm2_2 = "This amazing dish consists of a mashed potato base covered with a mixture of salmon, eggs, flour, onions, dried porcini mushrooms and other plants."
+var dialogueFarm2_3 = "Mmmm, actually, can you spare a few potatoes once you’re done?"
+
+var dialogueFish2_1 = "I could really go for some yukola right now."
+var dialogueFish2_2 = "The heavenly mixture of crushed dried salmon, red caviar, shiksha, pine, nuts, red bilberries, and other medicinal herbs always brightens my day."
+
+var dialogueHerd2_1 = "Oh, how I sometimes miss the good old days.  When I was a young man, I could take my reindeer to the same place every year to find food."
+var dialogueHerd2_2 = "Nowadays you youngins have to keep moving to new spots when rainfall freezes the snow above the lichen."
+
+var dialogueFarm3_1 = "Back in my day, farming was collectivized under the rule of the Soviet Union.  We had to rotate crops in order to feed ourselves every year."
+var dialogueFarm3_2 = "I remember having a third of my  family’s best potato crop taken by the state to feed soldiers fighting in uh, where was it, ah yes, Afghanistan."
+
+var dialogueFish3_1 = "Everything is so warm nowadays, even the ice isn’t as sturdy as it once was."
+var dialogueFish3_2 = "Our fishermen have to keep moving if they don’t want to end up in the river. But don’t worry, the river isn’t too deep (I hope)."
+
+var dialogueHerd3_1 = "While I do love caring for reindeer, their hide is amazingly warm."
+var dialogueHerd3_2 = "Every once in a while I would have to kill one of my reindeer for the village to use its meat and hide."
+var dialogueHerd3_3 = "Those were always sad days, but the reindeer’s lives were not in vain."
+
+var dialogueEnd_1 = "This is the worst (and only) wildfire I’ve ever seen!"
+
+var dialogueEndGood_1 = "The village is in ruins but due to your help we have enough resources to rebuild and recover!"
+var dialogueEndGood_2 = "If you are willing to continue helping, we will make sure you are rewarded appropriately."
+
+var dialogueEndBad_1 = "The village is in ruins and we don’t have enough resources to rebuild."
+var dialogueEndBad_2 = "Regional officials have been called and our people will be relocated to a safer location for the time being."
 
 var randomDialogue = [
 	"The Chawchu dance was one of my favorite things to watch and perform. I played the lalai drum, which I once broke by hitting it with my head.",
@@ -26,9 +61,9 @@ var usedDialogue = []
 func _ready():
 	
 	if scene == 1:
-		await play_dialogue(dialogue1_1)
+		await play_dialogue(dialogueFarm1_1)
 		print("next")
-		await play_dialogue(dialogue1_2)
+		await play_dialogue(dialogueFarm1_2)
 		print("next_scene")
 		
 		
