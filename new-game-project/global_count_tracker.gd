978d77e9796f5ffcf6173e628f866c2c467b8be9
lukebkastner <143747018+lukebkastner@users.noreply.github.com>
2025-03-16 09:29:57 -0400

    Round tracking

--- new-game-project/global_count_tracker.gd
+++ new-game-project/global_count_tracker.gd
@@ -2,8 +2,10 @@ extends Node
 
 var dialogue_count = 0
 var fishing_count = 0
-var reindeer_count = 0
+var reindeer_round_count = 0
 var farming_count = 0
+var reindeer_count = 5
+var lichen_count=8
 
 func get_dialogue_count():
 	dialogue_count += 1
@@ -13,10 +15,22 @@ func get_fishing_count():
 	fishing_count += 1
 	return fishing_count
 
+func get_reindeer_round_count():
+	reindeer_round_count += 1
+	return reindeer_round_count
+	
 func get_reindeer_count():
-	reindeer_count += 1
-	return reindeer_count
-		
+	return reindeer_count;
+	
+func set_reindeer_count(num):
+	reindeer_count=num
+
+func get_lichen_count():
+	return lichen_count
+
+func set_lichen_count(num):
+	lichen_count=num
+
 func get_farming_count():
 	farming_count += 1
 	return farming_count
