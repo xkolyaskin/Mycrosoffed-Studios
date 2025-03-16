8d147dde65b8bc86885e89fe451cec0c78c9f5c7
Thomas Ashcraft <termisdash@gmail.com>
2025-03-16 12:57:58 -0400

    fade to black with text at beginning

--- new-game-project/fade_to_black_special.gd
+++ new-game-project/fade_to_black_special.gd
@@ -7,6 +7,7 @@ func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	fade_rect.visible = false
 	fade_rect.modulate = Color(0, 0, 0, 0)
+	$Label.modulate = Color(255,255,255,0)
 
 func change_scene_with_fade(scene_path: String, duration := 1.0):
 	fade_rect.visible = true
@@ -16,12 +17,34 @@ func change_scene_with_fade(scene_path: String, duration := 1.0):
 	var fade_out = get_tree().create_tween().set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
 	fade_out.tween_property(fade_rect, "modulate:a", 1.0, duration)
 	await fade_out.finished
+	
+	#text
+	var fade = get_tree().create_tween().set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	$Label.modulate = Color(255,255,255,0)
+
+	
+	fade.tween_property($Label, "modulate:a", 1.0, duration)
+	await fade.finished
+	await get_tree().create_timer(5).timeout
+	
+	var fadeagain = get_tree().create_tween().set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	fadeagain.tween_property($Label, "modulate:a", 0.0, duration)
+	await fadeagain.finished
+
+	
+
+	
 	get_tree().change_scene_to_file(scene_path)
+	
 	await get_tree().process_frame
 	await get_tree().process_frame
-	# pause new loaded scene
 	get_tree().paused = true
 	
+	
+	
+
+	await get_tree().process_frame
+	await get_tree().process_frame
 	
 	
 	#fade back in
@@ -33,3 +56,7 @@ func change_scene_with_fade(scene_path: String, duration := 1.0):
 
 	fade_rect.visible = false
 	get_tree().paused = false
+
+	
+	
+	
