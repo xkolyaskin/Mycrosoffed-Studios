d01a0f8516a27ce62f06ce677343df7f4969a7bd
Thomas Ashcraft <termisdash@gmail.com>
2025-03-19 17:09:40 -0400

    hidden farms are unplantable

--- new-game-project/Visual Assets/Farming.gd
+++ new-game-project/Visual Assets/Farming.gd
@@ -6,14 +6,18 @@ var timer = 45
 var round
 
 func _ready():
-	round = GlobalCountTracker.get_farming_count() - 1
+	round = GlobalCountTracker.get_farming_count() + 1
 	await get_tree().create_timer(0.1).timeout
 	if round == 1:
 		$Farmland7.hide()
+		$Farmland7.set_plantable(false)
 	if round == 2:
 		$Farmland7.hide()
+		$Farmland7.set_plantable(false)
 		$Farmland4.hide()
+		$Farmland4.set_plantable(false)
 		$Farmland.hide()
+		$Farmland.set_plantable(false)
 		
 
 func _on_timer_timeout():
