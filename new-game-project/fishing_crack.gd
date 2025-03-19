0735e5370b0a404272a2fe819b6113db92536db3
Thomas Ashcraft <termisdash@gmail.com>
2025-03-19 10:56:51 -0400

    fishing crack changes

---
+++ new-game-project/fishing_crack.gd
@@ -0,0 +1,21 @@
+extends StaticBody2D
+
+@onready var player = get_node("/root/fishing_minigame/Player")
+
+var is_cracked = false
+var has_been_stepped_on = false
+
+func _physics_process(delta):
+	if is_cracked:
+		return
+	var overlapping = $HurtBox.get_overlapping_bodies()
+	if overlapping.size() >= 1:
+		has_been_stepped_on = true
+	if overlapping.size() == 0 and has_been_stepped_on and not is_cracked:
+		$IceHole.visible = true
+		set_collision_layer_value(1, true)
+		play_sound()
+		is_cracked = true
+
+func play_sound():
+	$"Water-splash-46402".play()
