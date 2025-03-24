8f86fb871cfb893e5bd2c5ae507f4a967a0cfe75
mill3103 <mill3103@purdue.edu>
2025-03-24 19:35:19 -0400

    improved credits scrolling, enlarged farmland hitbox for easier playing, expanded area of fire to work with bigger aspect ratio

--- new-game-project/credits.gd
+++ new-game-project/credits.gd
@@ -1,13 +1,13 @@
 extends Node2D
-var height = -2200
-var time = 30
+var height = -2700
+var time = 170
 
 func _ready() -> void:
 	position=Vector2(0,0)
 	await get_tree().create_timer(3).timeout
 	var pms = float(-1*height)/(time*10)
 	for i in range(0,time*10):
-		await get_tree().create_timer(0.1).timeout
+		await get_tree().create_timer(0.01).timeout
 		position=position+Vector2(0,0-pms)
 	GlobalCountTracker.reset()
 	FadeToBlack.change_scene_with_fade("res://menu.tscn")
